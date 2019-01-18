fd39aa79f6e28cfc89203aa36d60718c8af9c8f3
pradel <pradel.leo@gmail.com>
2019-01-18 16:46:49 +0100

    Improve login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { Button, Typography, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import * as blockstack from 'blockstack';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  loginButton: {
+    marginTop: theme.spacing.unit * 2,
+  },
+}));
+
 export const Login = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Typography>Looks like you are not logged in</Typography>
-      <Button onClick={() => blockstack.redirectToSignIn()}>Login</Button>
-    </div>
+    <Grid container>
+      <Grid item xs={12} className={classes.container}>
+        <Typography>Looks like you are not logged in</Typography>
+        <div>
+          <Button
+            className={classes.loginButton}
+            onClick={() => blockstack.redirectToSignIn()}
+            variant="outlined"
+            color="primary"
+          >
+            Login
+          </Button>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
