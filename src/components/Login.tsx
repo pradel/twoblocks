e97897189f4dd544238f1d9a52a96a995fabbc10
Pradel Léo <pradel.leo@gmail.com>
2019-01-23 21:42:07 +0100

    Improve login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Button, Typography, Grid } from '@material-ui/core';
+import { Button, Typography, Grid, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { useWindowSize } from 'the-platform';
 import * as blockstack from 'blockstack';
+
+const LinkAny: any = Link;
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -15,15 +18,46 @@ const useStyles = makeStyles(theme => ({
   loginButton: {
     marginTop: theme.spacing.unit * 2,
   },
+  image: {
+    height: 130,
+    maxWidth: '100%',
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
 }));
 
 export const Login = () => {
   const classes = useStyles();
+  const { height } = useWindowSize();
 
   return (
-    <Grid container>
+    <Grid container style={{ height }}>
       <Grid item xs={12} className={classes.container}>
-        <Typography>Looks like you are not logged in</Typography>
+        <Typography variant="h6">Twoblocks</Typography>
+
+        <img src="/undraw_authentication_fsn5.svg" className={classes.image} />
+
+        <Typography variant="body1">
+          Free and{' '}
+          <LinkAny
+            href="https://github.com/pradel/twoblocks"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open source
+          </LinkAny>{' '}
+          2fa manager
+          <br />
+          built with{' '}
+          <LinkAny
+            href="https://blockstack.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Blockstack
+          </LinkAny>
+        </Typography>
+
         <div>
           <Button
             className={classes.loginButton}
@@ -31,7 +65,7 @@ export const Login = () => {
             variant="outlined"
             color="primary"
           >
-            Login
+            Get started
           </Button>
         </div>
       </Grid>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="react-scripts" />
 
 declare module 'blockstack';
+declare module 'the-platform';
